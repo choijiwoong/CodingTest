ccbff04b83d660ee9a70d0a2c2bfdb5443e4dc15
최지웅 <gogogi313@naver.com>
2026-01-28 20:40:01 +0900

    feat. StringTokenizer와 InputStreamReader - InputStreamReader도 생성해야하기에 BufferedReader인자 안에 new써야함 - StringTokenizer의 nextToken은 공백 기반 split한 문자열이기에 Interger.parseInt()해야함. - bw.write에는 문자열 하나만 인자로 받음.

---
+++ 백준 단계별로 풀어보기/3. 반복문/11021.java
@@ -0,0 +1,25 @@
+import java.lang.System;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int length = Integer.parseInt(br.readLine());
+        for(int i=0; i<length; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write("Case #"+(i+1)+": "+(a+b)+"\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
