29978028b64eee9c8935c8eac240202cdfb58377
최지웅 <gogogi313@naver.com>
2026-02-02 12:27:08 +0900

    feat. 문자열을 char로 바꿀 땐 .toCharArray()

---
+++ 백준 단계별로 풀어보기/5. 문자열/11720.java
@@ -0,0 +1,18 @@
+import java.lang.System;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        char[] chars = br.readLine().toCharArray();
+        int sum = 0;
+        for (int i=0; i<N; i++)
+            sum += chars[i] - '0';
+
+        System.out.println(sum);
+        br.close();
+    }
+}
