3392567a5dd67824b16dab9fbb1ad1056bd87b88
최지웅 <gogogi313@naver.com>
2026-02-02 14:45:21 +0900

    feat. switch 도 충분히 빠르다.

---
+++ 백준 단계별로 풀어보기/5. 문자열/5622.java
@@ -0,0 +1,32 @@
+import java.lang.*;
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char[] input = sc.next().toCharArray();
+        int total_time = 0;
+        for(int i=0; i<input.length; i++){
+            char c = input[i];
+            switch(c){
+                case 'A': case 'B': case 'C':
+                    total_time+=3; break;
+                case 'D': case 'E': case 'F':
+                    total_time+=4; break;
+                case 'G': case 'H': case 'I':
+                    total_time+=5; break;
+                case 'J': case 'K': case 'L':
+                    total_time+=6; break;
+                case 'M': case 'N': case 'O':
+                    total_time+=7; break;
+                case 'P': case 'Q': case 'R': case 'S':
+                    total_time+=8; break;
+                case 'T': case 'U': case 'V':
+                    total_time+=9; break;
+                case 'W': case 'X': case 'Y': case 'Z':
+                    total_time+=10; break;
+            }
+        }
+        System.out.println(total_time);
+    }
+}
