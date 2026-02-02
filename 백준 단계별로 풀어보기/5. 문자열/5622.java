7bc6b4dc412b1be9f39bde3336045da83e9676b0
최지웅 <gogogi313@naver.com>
2026-02-02 14:53:32 +0900

    feat. br.readLine()의 종료는 반환값이 null인지 확인. - bw.write 시 줄바꿈은 별도의 bw.newLine()으로 최적화 호출.

--- 백준 단계별로 풀어보기/5. 문자열/5622.java
+++ 백준 단계별로 풀어보기/5. 문자열/5622.java
@@ -1,22 +1,19 @@
 import java.lang.*;
-import java.util.*;
-import java.io.IOException;
+import java.io.*;
 
 class Main{
     public static void main(String[] args) throws IOException{
-        int total_time = 0;
-        int c;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while ((c = System.in.read()) >= 'A') {
-            if (c <= 'C') total_time += 3;
-            else if (c <= 'F') total_time += 4;
-            else if (c <= 'I') total_time += 5;
-            else if (c <= 'L') total_time += 6;
-            else if (c <= 'O') total_time += 7;
-            else if (c <= 'S') total_time += 8;
-            else if (c <= 'V') total_time += 9;
-            else if (c <= 'Z') total_time += 10;
+        String line;
+        while((line = br.readLine()) != null) {
+            bw.write(line);
+            bw.newLine();
         }
-        System.out.println(total_time);
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
