757394c4f6a4815a4f87af98366ea8b9c3eb5d8c
최지웅 <gogogi313@naver.com>
2026-02-06 09:25:19 +0900

    feat. String의 split은 정규표현식을 받기에 .을 기준으로 나누려면 \\.을 사용해야한다. - 정규표현식 .은 전체 문자를 기준으로 하기에 비어버린다. - String은 repeat을 지원한다.

--- 백준 단계별로 풀어보기/7. 2차원 배열/10798.java
+++ 백준 단계별로 풀어보기/7. 2차원 배열/10798.java
@@ -2,27 +2,41 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
 class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuffer inputs = new StringBuffer();
+
+        // 1. 여러 줄 입력 받기
         String curr;
         while((curr = br.readLine())!=null){
             inputs.append(curr);
-            inputs.append('\n');
+            // 각 줄을 .으로 구분
+            inputs.append(".");
         }
-        String[] input = inputs.toString().split('\n');
-        int max_cols = input[0].length;
-        char[][] data;
-        for(int i=0; i<input.length; i++){
-            data[i] = input[i].toCharArray();
+        //2. 최대 길이의 열 찾기
+        String[] input = inputs.toString().split("\\.");
+        int N = input.length;// 입력 줄 개수
+        int max_cols=0;
+        for(int i=0; i<N; i++){
+            // 각 줄별 col 계산
+            int cols = input[i].length();
+            if(cols > max_cols)
+                max_cols = cols;
         }
-
+        //3. 최대 길이 기준으로 열 크기 맞추기
+        for(int i=0; i<N; i++){
+            if(input[i].length()<max_cols){
+                input[i]+=".".repeat(max_cols-input[i].length());
+            }
+        }
+        //4. 인덱스 기반 역순 출력하기
         for(int i=0; i<max_cols; i++){
-            for(int j=0; j<data.length; j++){
-                bw.write(data[j][i]);
+            for(int j=0; j<N; j++){
+                if(input[j].charAt(i) != '.'){
+                    bw.write(String.valueOf(input[j].charAt(i)));
+                }
             }
         }
         bw.flush();
