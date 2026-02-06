1a65b41dc685807c7b3807c0f7556d4d1b520b6a
최지웅 <gogogi313@naver.com>
2026-02-06 16:34:25 +0900

    refactor. N만큼 단순 반복 시 while (n-- > 0)사용. - 가급적 입력과 동시에 처리 가능한 것은 바로 처리

--- 백준 단계별로 풀어보기/7. 2차원 배열/2563.java
+++ 백준 단계별로 풀어보기/7. 2차원 배열/2563.java
@@ -3,36 +3,26 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    public static int[][] board = new int[100][100];
-
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int N = Integer.parseInt(br.readLine());
-        for(int i=0; i<N; i++){
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            fill_square(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
-        }
-        System.out.println(get_area());
-        br.close();
-    }
+        boolean[][] board = new boolean[100][100];
+        int total_area = 0;
 
-    public static void fill_square(int x, int y){
-        for(int i=0; i<10; i++){
-            for(int j=0; j<10; j++){
-                if(board[x+i][y+j]==0){
-                    board[x+i][y+j]=1;
+        while(N-- >0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            for(int i=x; i<x+10; i++){
+                for(int j=y; j<y+10; j++){
+                    if(!board[i][j]){
+                        board[i][j]=true;
+                        total_area++;
+                    }
                 }
             }
         }
-    }
-
-    public static int get_area(){
-        int sum=0;
-        for(int i=0; i<100; i++)
-            for(int j=0; j<100; j++)
-                if(board[i][j]==1)
-                    sum++;
-        return sum;
+        System.out.println(total_area);
     }
 }
