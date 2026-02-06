4736cc0789c921539bb483520ae3cf1fabecc4dc
최지웅 <gogogi313@naver.com>
2026-02-06 09:34:01 +0900

    refactor. 문제를 잘 보면 이차원 입력 시 N과 M을 알 수 있다. - char배열의 초기값은 '\0'이다. null을 의미하지만 char형의 null표현이기에 다르다.

--- 백준 단계별로 풀어보기/7. 2차원 배열/10798.java
+++ 백준 단계별로 풀어보기/7. 2차원 배열/10798.java
@@ -5,42 +5,26 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuffer inputs = new StringBuffer();
+        int N = 5, M = 15;
+        char[][] array = new char[N][M];
 
-        // 1. 여러 줄 입력 받기
-        String curr;
-        while((curr = br.readLine())!=null){
-            inputs.append(curr);
-            // 각 줄을 .으로 구분
-            inputs.append(".");
-        }
-        //2. 최대 길이의 열 찾기
-        String[] input = inputs.toString().split("\\.");
-        int N = input.length;// 입력 줄 개수
-        int max_cols=0;
         for(int i=0; i<N; i++){
-            // 각 줄별 col 계산
-            int cols = input[i].length();
-            if(cols > max_cols)
-                max_cols = cols;
+            String line = br.readLine();
+            if (line == null) break;
+
+            for(int j=0; j<line.length(); j++)
+                array[i][j]=line.charAt(j);
         }
-        //3. 최대 길이 기준으로 열 크기 맞추기
-        for(int i=0; i<N; i++){
-            if(input[i].length()<max_cols){
-                input[i]+=".".repeat(max_cols-input[i].length());
+
+        StringBuilder sb = new StringBuilder();
+        for(int j=0; j<M; j++){
+            for(int i=0; i<N; i++){
+                if(array[i][j] == '\0')
+                    continue;
+                sb.append(array[i][j]);
             }
         }
-        //4. 인덱스 기반 역순 출력하기
-        for(int i=0; i<max_cols; i++){
-            for(int j=0; j<N; j++){
-                if(input[j].charAt(i) != '.'){
-                    bw.write(String.valueOf(input[j].charAt(i)));
-                }
-            }
-        }
-        bw.flush();
-        bw.close();
+        System.out.println(sb.toString());
         br.close();
     }
 }
