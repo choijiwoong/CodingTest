3f55f7e8550accef52badddf87e88b97d8727681
최지웅 <gogogi313@naver.com>
2026-02-07 21:24:26 +0900

    refactor. Math.pow()는 Double연산이고 느린 연산이기에 multiplier =1해서 직접 loop마다 곱하자. - String().toCharArray()를 쓸 바에야 String.chatAt(i)를 쓰자

--- 백준 단계별로 풀어보기/8. 수학 1/2745.java
+++ 백준 단계별로 풀어보기/8. 수학 1/2745.java
@@ -6,18 +6,22 @@ class Main{
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         String input = st.nextToken();
         int B = Integer.parseInt(st.nextToken());
-        char[] array = input.toCharArray();
-        int length = array.length;
+
         int result=0;
-        for(int i=0; i<length; i++){
-            char c = array[length-1-i];
-            if (c>='0' && c<='9'){
-                result+=Math.pow(B, i)*(c-'0');
-            } else if (c>='A' && c<='Z'){
-                result+=Math.pow(B, i)*(c-'A'+10);
-            }
+        int multiplier = 1;
+
+        for(int i=input.length()-1; i>=0; i--){
+            char c= input.charAt(i);
+            int value;
+            if(c>='0' && c<='9')
+                value = c-'0';
+            else
+                value = c-'A'+10;
+            result+=value*multiplier;
+            multiplier*=B;
         }
         System.out.println(result);
         br.close();
