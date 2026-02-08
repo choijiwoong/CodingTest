38b8e60a9af14c474c82a1a6e5d0727d6011acc1
최지웅 <gogogi313@naver.com>
2026-02-08 19:15:36 +0900

    feat. 입력은 int여도 +혹은 * 시 long사용하자. 기준 21억. - for문에서만 multiplier/=std했다고 안주하지말고 다같이 줄어들어야 하는 수 상관관계 놓치지 말기

---
+++ 11005.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int number = Integer.parseInt(st.nextToken());
+        int std = Integer.parseInt(st.nextToken());
+        if(number==0){
+            System.out.println(0);
+            return;
+        }
+
+        long multiplier = 1;
+        while(number/multiplier>=1)
+            multiplier *= std;
+        multiplier/=std;
+
+        StringBuilder sb = new StringBuilder();
+        for(;multiplier>0; multiplier/=std){
+            long value = number/multiplier;
+            if(value>=1 && value<=9)
+                sb.append(String.valueOf(value));
+            else if(value>=10)
+                sb.append(String.valueOf((char)(value-10+'A')));
+            else
+                sb.append("0");
+            number%=multiplier;
+        }
+        System.out.println(sb.toString());
+        br.close();
+    }
+}
