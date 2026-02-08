e273aab30055506deca3fd8c78317835905eb4e1
최지웅 <gogogi313@naver.com>
2026-02-08 19:24:52 +0900

    refactor. 필요한 변수만 쓰기..

--- 11005.java
+++ 11005.java
@@ -8,27 +8,17 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int number = Integer.parseInt(st.nextToken());
         int std = Integer.parseInt(st.nextToken());
-        if(number==0){
-            System.out.println(0);
-            return;
-        }
-
-        long multiplier = 1;
-        while(number/multiplier>=1)
-            multiplier *= std;
-        multiplier/=std;
 
         StringBuilder sb = new StringBuilder();
-        for(;multiplier>0; multiplier/=std){
-            long value = number/multiplier;
-            if(value>=1 && value<=9)
-                sb.append(String.valueOf(value));
-            else if(value>=10)
-                sb.append((char)(value-10+'A'));
+        while(number>0){
+            int remainer = number%std;
+            if(remainer<10)
+                sb.append(remainer);
             else
-                sb.append("0");
-            number%=multiplier;
+                sb.append((char)(remainer-10+'A'));
+            number/=std;
         }
+
         System.out.println(sb.toString());
         br.close();
     }
