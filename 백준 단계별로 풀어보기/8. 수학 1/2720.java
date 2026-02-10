8b9b45efe8f2e730d3d4d4d0f66fdc7fc7ce2158
최지웅 <gogogi313@naver.com>
2026-02-10 10:32:06 +0900

    refactor. for에서 (int n : COINS)가 됨. - 상수는 private static final int - stringbuilder를 그냥 system.out해도 출력된다

--- 백준 단계별로 풀어보기/8. 수학 1/2720.java
+++ 백준 단계별로 풀어보기/8. 수학 1/2720.java
@@ -3,22 +3,23 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
+    private static final int[] COINS = {25,10,5,1};
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
         int n = Integer.parseInt(br.readLine());
-        int price;
-        int[] list = {25,10,5,1};
+
         while(n-- > 0){
-            price = Integer.parseInt(br.readLine());
-            for(int i=0; i<list.length; i++){
-                bw.write(price/list[i]+" ");
-                price%=list[i];
+            int price = Integer.parseInt(br.readLine());
+            for(int coin : COINS){
+                sb.append(price/coin).append(" ");
+                price%=coin;
             }
-            bw.newLine();
+            sb.append("\n");
         }
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
         br.close();
     }
 }
