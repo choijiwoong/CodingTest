740b24d9f2ca0bd9a622a40fc8109c1fa877def7
최지웅 <gogogi313@naver.com>
2026-02-14 00:07:26 +0900

    feat. 논리적 디버깅 필요. 99x101같은 경우 99만 뜸

---
+++ 백준 단계별로 풀어보기/9. 배수와 약수/11653.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int num = Integer.parseInt(br.readLine());
+        if (num<=3){
+            bw.write(String.valueOf(num));
+        } else{
+            for(int i=2; i*i<=num; i++){
+                while(num%i==0){
+                    num/=i;
+                    bw.write(String.valueOf(i));
+                    bw.newLine();
+                }
+            }
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
