c62fd46523634a57fcee5efb71463599458a2f22
최지웅 <gogogi313@naver.com>
2026-02-14 09:40:32 +0900

    refactor. 틀린이유를 모르면 문제를 쳐 읽자. 난독증인가?

--- 백준 단계별로 풀어보기/9. 배수와 약수/11653.java
+++ 백준 단계별로 풀어보기/9. 배수와 약수/11653.java
@@ -7,10 +7,12 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int num = Integer.parseInt(br.readLine());
-        if (num<=3){
+        if(num==1){
+
+        } else if (num<=3){
             bw.write(String.valueOf(num));
         } else{
-            for(int i=2; i*i<=num; i++){
+            for(int i=2; i<=num; i++){
                 while(num%i==0){
                     num/=i;
                     bw.write(String.valueOf(i));
