7001051ede840a8d6defdbbe062477b61d66ed84
최지웅 <gogogi313@naver.com>
2026-02-18 23:17:11 +0900

    style. 코드 주석 정리. 예외케이스 탐색 중

--- 백준 단계별로 풀어보기/10. 기하 1 - 직사각형과 삼각형/3009.java
+++ 백준 단계별로 풀어보기/10. 기하 1 - 직사각형과 삼각형/3009.java
@@ -8,6 +8,7 @@ class Main {
     }
 
     public static void main(String[] args) throws IOException{
+        // 1. 주어지는 점의 개수 3개. 입력 좌표를 x y 분리해서 배열 저장.
         int SIZE = 3;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] xs = new int[SIZE];
@@ -17,15 +18,18 @@ class Main {
             xs[i] = Integer.parseInt(st.nextToken());
             ys[i] = Integer.parseInt(st.nextToken());
         }
-        //피타고라스로 직각점을 구한 후, 다른 두 점의 중점으로부터 xy대칭
 
+        //2. 피타고라스로 직각점을 구한 후, 다른 두 점의 중점으로부터 xy대칭
         for(int i=0; i<SIZE; i++){
+            // i가 직각점일 때, 다른 두 점을 별도 변수로 저장
             int point2_x = xs[(i+1)%SIZE];
             int point2_y = ys[(i+1)%SIZE];
             int point3_x = xs[(i+2)%SIZE];
             int point3_y = ys[(i+2)%SIZE];
+            // 피타고라스 법칙을 이용하여 직각점이 올바른지 검증.
             int sum_right_sides = get_distance_multiple(xs[i],ys[i], point2_x, point2_y)
                     + get_distance_multiple(xs[i], ys[i], point3_x, point3_y);
+            // 만약 현재 i번째가 직각점(피타고라스 법칙에 해당)이라면, 다른 두 점의 중점을 기준으로 직각점을 대칭.
             if(sum_right_sides == get_distance_multiple(point2_x, point2_y, point3_x, point3_y)){
                 int mid_x = (point2_x+point3_x)/2;
                 int mid_y = (point2_y+point3_y)/2;
