b305805b43212d4f9c61f8081e84aec14f6996be
최지웅 <gogogi313@naver.com>
2026-02-21 21:07:45 +0900

    debug. int는 약 21억까지

--- 백준 단계별로 풀어보기/10. 기하 1 - 직사각형과 삼각형/15894.java
+++ 백준 단계별로 풀어보기/10. 기하 1 - 직사각형과 삼각형/15894.java
@@ -6,7 +6,10 @@ class Main{
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int depth = Integer.parseInt(br.readLine());
-        System.out.println(depth*4);
+        // 이때 depth의 범위가 10^9승으로 10억. int는 약 20억까지 된다.
+        // 하지만 4배를 하면 범위가 초과되기에 버퍼 오버플로우가 발생할 것이다.
+        // 4에 L을 붙여 Long으로 변환시켜보자.
+        System.out.println(depth*4L);
         br.close();
     }
 }
