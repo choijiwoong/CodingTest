3811ca78f3f2c506b26f094f584769f58acca0fe
최지웅 <gogogi313@naver.com>
2026-03-01 20:14:54 +0900

    debug. 접근법을 잘 모르겠다. 초기에 n==1일 때 실행횟수 계수가 0이 아닐까했는데 오답. 혹시몰라 1로 했는데도 오답.

--- 백준 단계별로 풀어보기/11. 시간복잡도/24264.java
+++ 백준 단계별로 풀어보기/11. 시간복잡도/24264.java
@@ -8,13 +8,20 @@ class Main {
             O(n^2)
             입력값 제곱.
             차수는 계수가 아니라 지수를 말하는거였나봄
+
+            왜 틀렸나 생각해보니, 만약 n이 1이라면?
+            예시의 코드가 의사코드여서 확실친 않지만 1 to n에서 n==1일 때 반복문이 수행되지 않는다면
+            n==1이면 수행횟수가 2가 아닌 0이 될 것이다. 해당 부분만 if 로 빼보자.
         */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int input = Integer.parseInt(br.readLine());
         bw.write(String.valueOf(input*input));
         bw.newLine();
-        bw.write("2");
+        if (input!=1)
+            bw.write("2");
+        else
+            bw.write("0");
         bw.flush();
         br.close();
         bw.close();
