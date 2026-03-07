79a2ae9d601cad911800ab73f8b483bada934f74
최지웅 <gogogi313@naver.com>
2026-03-07 20:38:26 +0900

    feat. 미루다가 여행와서 품

--- 백준 단계별로 풀어보기/12. 브루트 포스/2798.java
+++ 백준 단계별로 풀어보기/12. 브루트 포스/2798.java
@@ -3,7 +3,7 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         /*
             양의정수씌여진 카드 N개에서 3개를 골라서 합 M이하 최댓값 만들기.
             합의 범위는 최대 300,000으로 int범위로 가능
@@ -14,6 +14,7 @@ class Main {
         StringTokenizer st =new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        int SIZE = 3;
         int[] cards = new int[N];
 
         st = new StringTokenizer(br.readLine());
@@ -21,5 +22,30 @@ class Main {
             cards[i] = Integer.parseInt(st.nextToken());
         Arrays.sort(cards);
         //일단 원소 중 M을 넘는 수는 불필요.
+        //1~M까지의 카드 중 3장을 모두 골랐을 때 최대 값이 되는 합을 출력.
+        //3중 for을 쓸건데, 만약 3개원소 뿐이라면 필히 그 합이 답이 된다.
+
+        int result=0, sum=0;
+
+        if(N==SIZE){
+            result = cards[0] + cards[1] + cards[2];
+        } else{
+            for(int i=0; i<N-2; i++){
+                for(int j=i+1; j<N-1; j++){
+                    for(int k=j+1; k<N; k++){
+                        sum = cards[i]+cards[j]+cards[k];
+                        if(sum>M){
+                            break;
+                        } else if(sum>result){
+                            result = sum;
+                        }
+                    }
+                }
+            }
+        }
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
