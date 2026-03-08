fbe02bb9a2dd47d4fc8ed091796632897805ecd5
최지웅 <gogogi313@naver.com>
2026-03-08 23:47:45 +0900

    feat. 디버깅 필요

---
+++ 백준 단계별로 풀어보기/12. 브루트 포스/2231.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static int get_prev_constructor(int num){
+        int result = num;
+        for(char c : String.valueOf(num).toCharArray())
+            result-=c-'0';
+        return result;
+        /*
+            정의를 다시 읽어봐라. 단순 자릿수 뺼셈이 아니라 self + self.split.sum() = next 인 구조다. 즉
+            num보다 작은 수들 중 n+n.split.sum()이 num과 같은 수를 찾으면 된다.
+        */
+    }
+
+    public static void main(String[] args) throws IOException{
+        /*
+            N의 생성자 M : M+M의 각 자리수 합이 N인 경우
+            245의 분해합은 245+2+4+5 = 256, 245는 256의 생성자.
+            생성자가 존재하지 않거나, 여러개인 자연수도 존재.
+
+            N이 주어졌을때 생성자 중 가장 작은 값을 계산.
+            생성자가 없을 경우 0출력
+
+            각 자리수를 역으로 빼면서 최소의 생성자를 계산.
+            입력은 1,000,000까지의 자연수 범위이니 int로 해결 가능.
+            각 자리수를 빼서 생성자로 만드는 작업을 별도의 함수로 분리.
+        */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int input = Integer.parseInt(br.readLine());
+
+        int prev_num = input;
+        while(input>0){
+            prev_num = input;
+            input = get_prev_constructor(input);
+        }
+
+        bw.write(String.valueOf(prev_num));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
