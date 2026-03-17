25b1b85714e60bcb51e9aa4e0c5d07264ac9e70a
최지웅 <gogogi313@naver.com>
2026-03-17 18:06:27 +0900

    debug. StringTokenizer 대신. toCharArray 사용. 조건 오류 디버깅 필요.

--- 백준 단계별로 풀어보기/12. 브루트 포스/1018.java
+++ 백준 단계별로 풀어보기/12. 브루트 포스/1018.java
@@ -3,10 +3,16 @@ import java.lang.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws IOException{
-        /*
+    public static void print_board(int[][] board, int N, int M){
+        for(int i=0; i<N; i++){
+            for(int j=0; j<M; j++){
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
 
-         */
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -17,24 +23,28 @@ class Main {
         int[][] board = new int[N][M];
 
         for(int i=0; i<N; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j=0; j<M; j++){
-                //White를 1로 저장
-                String s = st.nextToken();
-                board[i][j] = s.equals("W")?1:0;
+            // StringTokenizer는 공백으로 구분된 문자열을 분해한다.
+            // 공백이 없는 문자열을 분해하려면 toCharArray()로 직접 접근해야함.
+            char[] line = br.readLine().toCharArray();
+            for(int j=0; j<line.length; j++){
+                board[i][j] = line[j]=='W'?1:0;
             }
         }
+
+        print_board(board, N, M);
 
         for(int i=0; i<N-WINDOW_SIZE; i++){
             for(int j=0; j<M-WINDOW_SIZE; j++){
                 //i,j의 칸을 기준으로 판단 기준으로
                 int count = 0;
+                int std = board[i][j];
+                //일단 아래에서 오류가 발생하는데 0,1 이면 합으로 한번에 계산도 가능할듯?
                 for(int ii=0; ii<WINDOW_SIZE; ii++){
                     for(int jj=0; jj<WINDOW_SIZE; jj++){
-                        int std = board[i][j];
-                        if(ii%2==0 && board[i+ii][j+jj]!=std){
+                        //
+                        if(((ii%2==0 && jj%2==0) || (ii%2==1 && jj%2==1)) && board[i+ii][j+jj]!=std){
                             count++;
-                        } else if(ii%2!=0 && board[i+ii][j+jj]==std){
+                        } else if(board[i+ii][j+jj]==std){
                             count++;
                         }
                     }
