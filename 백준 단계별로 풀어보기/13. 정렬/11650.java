b87a57f9a81b1149432ab8a1bb15b4987f4025d5
최지웅 <gogogi313@naver.com>
2026-03-31 11:03:33 +0900

    feat. Arrays.sort 뒤 람다 Comparator는 객체 배열만 받을 수 있다.

---
+++ 백준 단계별로 풀어보기/13. 정렬/11650.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] xs = new int[n];
+        int[] ys = new int[n];
+        int[] idxs = new int[n];
+
+        for(int i=0; i<n; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            xs[i] = Integer.parseInt(st.nextToken());
+            ys[i] = Integer.parseInt(st.nextToken());
+            idxs[i] = i;
+        }
+
+        //Arrays.sort()는 람다를 이용해 기준을 전달 가능하다.
+        //x를 기준으로 idxs 정렬
+        //y를 기준으로 x 중복에 한해 idxs 정렬
+        Arrays.sort(idxs, (i, j)->{
+            if(xs[i]!=xs[j])
+                return xs[i]-xs[j];
+            else
+                return ys[i]-ys[j];
+        });
+
+        //idxs순서대로 출력
+        for(int i : idxs){
+            bw.write(String.valueOf(xs[i]));
+            bw.write(" ");
+            bw.write(String.valueOf(ys[i]));
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
