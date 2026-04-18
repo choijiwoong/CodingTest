d35380f1b68769ac32d514a4272d10724f6c36c6
최지웅 <gogogi313@naver.com>
2026-04-18 23:12:48 +0900

    debug. 잘못올린 두 파일 swap

--- 백준 단계별로 풀어보기/15. 약수, 배수와 소수2/13909.java
+++ 백준 단계별로 풀어보기/15. 약수, 배수와 소수2/13909.java
@@ -10,10 +10,26 @@ class Main {
             1번째 사람은 1배수 index를 xor.
             2번째 사람은 2배수 index를 xor.
             초기에 모든 창문은 닫혀있음.
+
+            수학적 규칙이 있을 듯 하지만 한 눈에 알아보기 쉽지 않다.
+
+            시도 1.
+            늘 그렇듯 우선 brute force하게 TDD접근한 뒤 최적화를 시도해보자.
+            사실 간단하게만 생각하면 int[n]를 만들어서 직접 !시키는거다.
         */
         Scanner sc = new  Scanner(System.in);
         int n = sc.nextInt();
-        int result=0;
+
+        int[] window = new int[n+1];
+        for(int i=1; i<=n; i++){
+            //i번째 사람은
+            for(int j=1; i*j<=n; j++){
+                window[i*j] = (window[i*j]==1)?0:1;
+            }
+        }
+
+        int result = Arrays.stream(window).sum();
+
         System.out.println(result);
         sc.close();
     }
