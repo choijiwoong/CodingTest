f597dde401e3dc517deb1f07d6f6016dccb1db25
최지웅 <gogogi313@naver.com>
2026-04-25 23:57:54 +0900

    feat. 문제 이해 정리

---
+++ 백준 단계별로 풀어보기/33. 트리/11725.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) {
+        /*
+            문제 이해를 목적으로 하루 5분남기고 커밋하자.
+            이거 이전에 이해는 했던 문제다. 각 노드 별 부모 노드를 찾는 문제.
+            일차원적으로 생각나는 것은 여러 노드가 연결되어있고, 부모 노드만 찾으면 되기에
+            배열에 부모, 자신 꼴로 저장하면 될 듯 하다. 루트는 1이기에 순서는 찾을 수있을 듯 하다.
+            연결을 타고타고 가면서 각 숫자 별 부모만 DFS같이 방향성 있게 찾으면 된다.
+            아니면 레벨 별로 타고 내려가도 된다.
+
+        */
+        System.out.println("Hello world!");
+    }
+}
